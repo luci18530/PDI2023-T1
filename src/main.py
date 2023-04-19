86574ec30c0ae5dad3f69542e77d7b0c924e7ea6
Luciano Pereira <54913266+luci18530@users.noreply.github.com>
2023-04-18 22:50:30 -0300

    adicionando comentários

--- src/main.py
+++ src/main.py
@@ -69,46 +69,61 @@ YIQ_TO_RGB_MATRIX = np.array([
 if not args.no_original:
     display_handler(img_arr, 'Original Image')
 
+# Realiza uma operação de conversão de espaço de cores da imagem de RGB para YIQ e novamente de volta para RGB
 if args.yiq:
     #? TODO: podemos normalizar os valores?
-    img_arr_norm_yiq = np.dot(img_arr_norm.copy(), RGB_TO_YIQ_MATRIX.T)
-    img_arr_norm_rgb = np.dot(img_arr_norm_yiq, YIQ_TO_RGB_MATRIX.T)
-    img_arr_neg_rgb = np.clip(img_arr_norm_rgb * 255, 0, 255).round().astype(np.uint8)
-    display_handler(img_arr_neg_rgb, 'RGB-YIQ-RGB Image')
+    img_arr_norm_yiq = np.dot(img_arr_norm.copy(), RGB_TO_YIQ_MATRIX.T) # Cria uma cópia do array de imagem normalizado e multiplica o array de imagem  matriz de conversão de RGB para YIQ
+    img_arr_norm_rgb = np.dot(img_arr_norm_yiq, YIQ_TO_RGB_MATRIX.T) # Retorna para RGB
+    img_arr_neg_rgb = np.clip(img_arr_norm_rgb * 255, 0, 255).round().astype(np.uint8) # Multiplica o array por 255 e limita seus valores entre 0 e 255 usando a função np.clip()
+    display_handler(img_arr_neg_rgb, 'RGB-YIQ-RGB Image') # exibir
     save_handler(img_arr_neg_rgb, IMAGE_PATH, 'yiq')
 
+# Negativo em RGB
 if args.neg_rgb:
-    img_arr_neg_rgb = (255 - img_arr).astype(np.uint8)
+    img_arr_neg_rgb = (255 - img_arr).astype(np.uint8) 
     display_handler(img_arr_neg_rgb, 'RGB Negative Image')
     save_handler(img_arr_neg_rgb, IMAGE_PATH, 'neg_rgb')
 
+# Negativo em Y
 if args.neg_y:
     img_arr_norm_yiq = np.dot(img_arr_norm.copy(), RGB_TO_YIQ_MATRIX.T)
-    img_arr_norm_yiq[:, :, 0] = 1 - img_arr_norm_yiq[:, :, 0]
+    img_arr_norm_yiq[:, :, 0] = 1 - img_arr_norm_yiq[:, :, 0] # Inverte Y
     img_arr_norm_rgb = np.dot(img_arr_norm_yiq, YIQ_TO_RGB_MATRIX.T)
     img_arr_rgb_neg_y = np.clip(img_arr_norm_rgb * 255, 0, 255).round().astype(np.uint8)
     display_handler(img_arr_rgb_neg_y, 'Y Negative Image')
     save_handler(img_arr_rgb_neg_y, IMAGE_PATH, 'neg_y')
 
+# Verifica se a opção filter foi passada como argumento
 if args.filter:
+    # Itera sobre cada filtro passado como argumento
     for f in args.filter:
-        if f[0] == '[' and f[-1] == ']':
+        if f[0] == '[' and f[-1] == ']': # Se o elemento for uma string delimitada por colchetes [ e ], ele é tratado como uma função definida pelo usuário
             new_filter = filter.get_function_filter(f)
-        else:
+        else: # Caso contrário, considera que é um filtro em formato JSON e utiliza o método from_json do módulo filter para obter a instância do filtro
             new_filter = filter.DataFilter.from_json(f)
+        # Aplica o filtro na imagem, criando uma cópia da matriz de pixels
         img_arr_filtered = new_filter.apply(img_arr.copy())
         display_handler(img_arr_filtered, f'Filtered Image with {new_filter.name}')
         save_handler(img_arr_filtered, IMAGE_PATH, f'filter-{new_filter.name}')
 
+# Verifica se há uma sequência de filtros especificada nos argumentos
 if args.filter_sequence:
+    # Itera sobre a lista de sequências de filtros especificada nos argumentos
     for fs in args.filter_sequence:
+        # Faz uma cópia da imagem original para ser filtrada
         img_arr_filtered = img_arr.copy()
         for f in fs:
+            # Verifica se o filtro é uma função pré-definida
             if f[0] == '[' and f[-1] == ']':
+                # Obtém a função de filtro a partir da string especificada
                 new_filter = filter.get_function_filter(f)
+                
+            # Se não for uma função pré-definida, verifica se o formato de entrada dos filtros é JSON
             else:
+                # Cria um objeto de filtro de dados a partir da string JSON especificada
                 if args.input_format == 'json':
                     new_filter = filter.DataFilter.from_json(f)
+                # Se o formato de entrada dos filtros for TXT, cria um objeto de filtro de dados a partir da string TXT especificada
                 else:
                     new_filter = filter.DataFilter.from_txt(f)
             img_arr_filtered = new_filter.apply(img_arr_filtered)
